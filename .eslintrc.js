e0bda77797067603603502c0e8e0cde9e60d8762
marcochiaro <mchiaro79@gmail.com>
2022-04-12 17:00:07 -0300

    feat:add prittier and linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,26 @@
+const RULES = {
+  OFF: "off",
+  ERROR: "error",
+  WARN: "warn",
+};
+
 module.exports = {
   env: {
     browser: true,
     es2021: true,
   },
-  extends: ["airbnb-base"],
+  extends: ["plugin:react/recommended", "standard", "next", "prettier"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
     ecmaVersion: "latest",
     sourceType: "module",
   },
-  plugins: ["@typescript-eslint"],
-  rules: {},
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "react/prop-types": RULES.OFF,
+    "react/react-in-jsx-scope": RULES.OFF,
+  },
 };
